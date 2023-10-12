a3ceedde09bc7183f54ecc8ea3183503b670f14c
Jocelyn Masserot <jocemass@gmail.com>
2023-10-12 10:36:31 -0700

    Add cionic lib

---
+++ lib/cionic/diff_filter.c
@@ -0,0 +1,105 @@
+#include "shared-bindings/util.h"
+#include "diff_filter.h"
+#include <math.h>
+
+// diff_filter_run
+// --
+// given an array of floats <input>, return the <order>-order diff of position zero
+//
+// samples ordered most recent at [0]
+//
+// first order diff
+// out[i] = a[i] - a[i+1]
+//
+// second order diff
+// inter[i] = a[i] - a[i+1]
+// out[i] = inter[i] - iter[i+1]
+
+static float
+diff_filter_run(float *input, int order) {
+    if (order == 0) {
+        return input[0];
+    }
+    float interim[order];
+    for (int i = 0; i < order; i++) {
+        interim[i] = input[i] - input[i+1];
+    }
+    return diff_filter_run(interim, order-1);
+}
+
+// diff_filter_process
+// --
+// called on every EMG sample
+//
+// for each channel
+//   shift input buffer and append sample
+//   calculate N-order diff on that sample
+//   append to rms_buffer
+//
+// once rms_buffer is full for each channel
+//   calculate sum_of_squares for the rms_buffer
+//   shift rms_squares buffer and append sum_of_squares
+//   calculate RMS value from rm_squares buffer
+//   and send out over BLE
+//
+// process a single sample of emg data and get results
+int
+diff_filter_process(diff_filter_t *diff, float *norms, int numchans,
+                    elapsed_t ts_in, const uint8_t *buffer,
+                    elapsed_t *ts_out, float *uv_out)
+{
+    // 1. calculate N order diff and prepend to rms_bufffer
+    for (int i = 0; i < numchans; i++) {
+        // @todo replace with memmove
+        for (int j = DIFF_FILTER_ORDER; j > 0; j--) {
+            diff->input[i][j] = diff->input[i][j-1];
+        }
+        int24_t tf = READ_BE(int24_t, buffer);
+        float uv = (float)tf.value * norms[i];
+        diff->input[i][0] = uv;
+        buffer += 3;
+        // calculate N-order diff
+        float output = diff_filter_run(diff->input[i], DIFF_FILTER_ORDER);
+        // append to rms_buffer
+        diff->rms_buffer[i][diff->rms_samples] = output;
+        // output channel rms
+        uv_out[i] = diff->rms[i];
+    }    
+
+    // 2. if rms_buffer filled compute sos for window and update rms
+    if (++diff->rms_samples >= RMS_NUM_SAMPLES) {
+        for (int i = 0; i < numchans; i++) {
+            float square = 0;
+            for (int j = 0; j < RMS_NUM_SAMPLES; j++) {
+                float val = diff->rms_buffer[i][j];
+                float sq = val*val;
+                square += sq;
+            }
+            // compute rms
+            // start with newly compute square
+            // move each rms window down
+            // adding to the sum of squares
+            float sos = square;
+            for (int k = 1; k < RMS_NUM_SQUARES; k++) {
+                sos += diff->rms_squares[i][k];
+                diff->rms_squares[i][k-1] = diff->rms_squares[i][k];
+            }
+            diff->rms_squares[i][RMS_NUM_SQUARES-1] = square;
+            diff->rms[i] = sqrt(sos/(RMS_NUM_SQUARES*RMS_NUM_SAMPLES));
+            // update output rms
+            uv_out[i] = diff->rms[i];
+        }
+        diff->rms_samples = 0;
+    }
+
+    // output at same timestamp as input
+    *ts_out = ts_in;
+    return 0;
+}
+
+void
+diff_filter_init(diff_filter_t *filter)
+{
+    bzero(filter, sizeof(diff_filter_t));
+}
+
---
+++ lib/cionic/utils.h
@@ -0,0 +1,76 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdint.h>
+
+typedef uint32_t elapsed_t;
+
+#define PACKED __attribute__((packed))
+
+typedef struct PACKED int24_t {
+    signed value : 24;
+} int24_t;
+
+typedef struct PACKED uint24_t {
+    unsigned value : 24;
+} uint24_t;
+
+// returns value of TYPE from uint8_t INPTR (advancing it), reversing endianness if REVERSE
+#define _READ_ENDIAN_ADVPTR(TYPE, INPTR, __REVERSE) ({ \
+    TYPE val; \
+    uint8_t *valptr = (uint8_t *) &val; \
+    if (__REVERSE) { \
+        if (sizeof(TYPE) >= 8) { valptr[7] = *INPTR++; } \
+        if (sizeof(TYPE) >= 7) { valptr[6] = *INPTR++; } \
+        if (sizeof(TYPE) >= 6) { valptr[5] = *INPTR++; } \
+        if (sizeof(TYPE) >= 5) { valptr[4] = *INPTR++; } \
+        if (sizeof(TYPE) >= 4) { valptr[3] = *INPTR++; } \
+        if (sizeof(TYPE) >= 3) { valptr[2] = *INPTR++; } \
+        if (sizeof(TYPE) >= 2) { valptr[1] = *INPTR++; } \
+                                 valptr[0] = *INPTR++;   \
+    } else { \
+        val = *(const TYPE *) INPTR; \
+        INPTR += sizeof(TYPE); \
+    } \
+    val; \
+})
+
+// returns value of TYPE from PTR, reversing endianness if REVERSE
+#define _READ_ENDIAN(TYPE, PTR, __REVERSE) ({    \
+    const uint8_t *inptr = (uint8_t *) (PTR);    \
+    _READ_ENDIAN_ADVPTR(TYPE, inptr, __REVERSE); \
+})
+
+// writes VAL of TYPE to PTR, reversing endianness if REVERSE
+#define _WRITE_ENDIAN(TYPE, PTR, VAL, REVERSE) ({ \
+    TYPE val = VAL; \
+    uint8_t *valptr = (uint8_t *) &val; \
+    uint8_t *outptr = (uint8_t *) (PTR); \
+    if (REVERSE) { \
+        if (sizeof(TYPE) >= 8) { *outptr++ = valptr[7]; } \
+        if (sizeof(TYPE) >= 7) { *outptr++ = valptr[6]; } \
+        if (sizeof(TYPE) >= 6) { *outptr++ = valptr[5]; } \
+        if (sizeof(TYPE) >= 5) { *outptr++ = valptr[4]; } \
+        if (sizeof(TYPE) >= 4) { *outptr++ = valptr[3]; } \
+        if (sizeof(TYPE) >= 3) { *outptr++ = valptr[2]; } \
+        if (sizeof(TYPE) >= 2) { *outptr++ = valptr[1]; } \
+                                 *outptr++ = valptr[0];   \
+    } else { \
+        *(TYPE *) outptr = val; \
+        outptr += sizeof(TYPE); \
+    } \
+})
+
+
+// These depend on the actual endianness of the system.
+// BIG_ENDIAN == 0 == !LITTLE_ENDIAN since our systems are all little-endian, 
+#define CIONIC_BIG_ENDIAN 0
+#define CIONIC_LITTLE_ENDIAN 1
+
+#define READ_LE(TYPE, PTR) _READ_ENDIAN(TYPE, PTR, CIONIC_BIG_ENDIAN)
+#define READ_BE(TYPE, PTR) _READ_ENDIAN(TYPE, PTR, CIONIC_LITTLE_ENDIAN)
+
+#define WRITE_LE(TYPE, PTR, VAL) _WRITE_ENDIAN(TYPE, PTR, VAL, CIONIC_BIG_ENDIAN)
+#define WRITE_BE(TYPE, PTR, VAL) _WRITE_ENDIAN(TYPE, PTR, VAL, CIONIC_LITTLE_ENDIAN)
+
+#endif //#ifndef __UTILS_H__
