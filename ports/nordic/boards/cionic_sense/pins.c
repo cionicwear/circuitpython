49cc6d90d27dfa33b581af371cdc1b16260c56c2
Jeremiah Robison <jerobi@cionic.com>
2024-07-29 10:33:59 -0700

    fix UART / D6 D7 pin mappings

--- ports/nordic/boards/cionic_sense/pins.c
+++ ports/nordic/boards/cionic_sense/pins.c
@@ -16,8 +16,8 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_D3), MP_ROM_PTR(&pin_P0_29) },
     { MP_ROM_QSTR(MP_QSTR_D4), MP_ROM_PTR(&pin_P0_04) },
     { MP_ROM_QSTR(MP_QSTR_D5), MP_ROM_PTR(&pin_P0_05) },
-    { MP_ROM_QSTR(MP_QSTR_D6), MP_ROM_PTR(&pin_P1_12) },
-    { MP_ROM_QSTR(MP_QSTR_D7), MP_ROM_PTR(&pin_P1_11) },
+    { MP_ROM_QSTR(MP_QSTR_D6), MP_ROM_PTR(&pin_P0_16) },
+    { MP_ROM_QSTR(MP_QSTR_D7), MP_ROM_PTR(&pin_P0_24) },
     { MP_ROM_QSTR(MP_QSTR_D8), MP_ROM_PTR(&pin_P1_09) },
     { MP_ROM_QSTR(MP_QSTR_D9), MP_ROM_PTR(&pin_P1_08) },
     { MP_ROM_QSTR(MP_QSTR_D10), MP_ROM_PTR(&pin_P0_08) },
@@ -50,8 +50,8 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SDA2), MP_ROM_PTR(&pin_P0_04) },
     { MP_ROM_QSTR(MP_QSTR_SCL2), MP_ROM_PTR(&pin_P0_05) },
 
-    { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_P1_12) },
-    { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_P1_11) },
+    { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_P0_16) },
+    { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_P0_24) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
